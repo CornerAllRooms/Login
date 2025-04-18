7c977de8d00b7a0b6e7a1a6f7ba94268c2ab2116
CornerAllRooms <165560207+CornerAllRooms@users.noreply.github.com>
2025-04-18 09:03:43 +0200

    Auth.php

--- Auth.php
+++ Auth.php
@@ -2,5 +2,5 @@
 session_start();
 // Your existing authentication code
 $_SESSION['logged_in'] = true; 
-header("Location: ../homepage.php"); // ← Connects to homepage
+header("Location:homepage.php"); // ← Connects to homepage
 ?>
